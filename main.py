13aefea1db90bf53ea8a5f4dedeea91527dccfb6
nslqqq <nslqqq+github2@gmail.com>
2012-03-30 23:49:32 +0400

    Added solution submitting function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python2
-# -*- coding: <encoding name> -*-
+# -*- coding: utf-8 -*-
 
+from poster.encode import multipart_encode
+from poster.streaminghttp import register_openers
+import poster
 import urllib2
 import urllib
 import cookielib
@@ -8,17 +11,19 @@ import cookielib
 class CodeforcesClient(object):
 	def __init__(self, login, password, debug_level=0):
 		""" Start up """
+		register_openers()
+
 		self.login = login
 		self.password = password
 
 		self.cj = cookielib.MozillaCookieJar()
 
-		self.opener = urllib2.build_opener(
-		  urllib2.HTTPRedirectHandler(),
-		  urllib2.HTTPHandler(debuglevel=debug_level),
-		  urllib2.HTTPSHandler(debuglevel=debug_level),
-		  urllib2.HTTPCookieProcessor(self.cj)
-		)
+		self.opener = poster.streaminghttp.register_openers()
+
+		self.opener.add_handler(urllib2.HTTPRedirectHandler())
+		self.opener.add_handler(urllib2.HTTPHandler(debuglevel=debug_level))
+		self.opener.add_handler(urllib2.HTTPSHandler(debuglevel=debug_level))
+		self.opener.add_handler(urllib2.HTTPCookieProcessor(self.cj))
 
 		self.opener.addheaders = [
 		  ('User-agent', ('Mozilla/5.0 (X11; Linux x86_64) '
@@ -33,7 +38,7 @@ class CodeforcesClient(object):
 		"""
 		response = self.opener.open("http://codeforces.com/")
 		return ''.join(response.readlines())
-	
+
 	def calc_tta(self, hstr):
 		"""
 		This calculates protection value (_tta)
@@ -55,14 +60,14 @@ class CodeforcesClient(object):
 	def get_tta(self):
 		"""
 		Returns tta for session
-		"""	
+		"""
 		tta = filter(lambda x: x.name == '39ce7', self.cj)[0].value
 		tta = self.calc_tta(tta)
 		return tta
 
 	def auth(self):
 		"""
-		Auth in codeforces 
+		Auth in codeforces
 		"""
 
 		# Retrive cookies
@@ -78,11 +83,33 @@ class CodeforcesClient(object):
 		})
 		self.opener.open("http://codeforces.com/enter/", login_data)
 
+	def get_language_id(self, file_name):
+		langs = {'java' : 23, 'cpp' : 16, 'py' : 7,	'cs' : 9}
+
+		for key, val in langs.items():
+			if file_name.endswith('.' + key):
+				return val
+
+	def submit_solution(self, contest_id, task_id, file_name):
+		data, headers = multipart_encode({
+				'submittedProblemIndex': task_id,
+				'programTypeId' : self.get_language_id(file_name),
+				'source' : '',
+				'_tta' : self.get_tta(),
+				'sourceFile' : open(file_name),
+				})
+		print data, headers
+		request = urllib2.Request("http://codeforces.com/contest/" + contest_id + "/submit/", data, headers)
+		request = self.opener.open(request)
+		print ''.join(request.readlines())
+
+
 if __name__ == '__main__':
 	settings = {}
 	for line in open(".cfclient"):
 		key, val = line.strip().split()
 		settings[key] = val
-	
+
 	client = CodeforcesClient(settings['login'], settings['pass'])
-	print client.get_index()
+	#print client.get_index()
+	client.submit_solution('166', 'B', './solutions/solution.cpp')
